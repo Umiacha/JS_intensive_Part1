a8bd74f90983ff7ed38c561398beb04e8aa026ec
Umiacha <umiacha@yandex.ru>
2025-08-26 19:56:04 +0300

    Add ddt-6.

---
+++ deep-dive-task/deep-dive-task-6/class-inheritance.js
@@ -0,0 +1,15 @@
+function Transport(year, color) {
+}
+
+function Car(brand, model, type, year, color) {
+}
+
+function Helicopter(model, engines, rotors, year, color) {
+}
+
+// Экспортируем для тестирования
+module.exports = {
+    Transport,
+    Car,
+    Helicopter
+};
