28112a0c8caaaa28b81cd6212eba75773b878a6b
Umiacha <umiacha@yandex.ru>
2025-08-26 19:44:55 +0300

    Del solution for ddt-1.

--- deep-dive-task/deep-dive-task-1/solution.js
+++ deep-dive-task/deep-dive-task-1/solution.js
@@ -1,38 +1,6 @@
-export const upperFirst = (str) => {
-    // TODO: установить зависимости (через впн) и привести в соответствие с тестами!
-    let arr = Array(...str);
-    let words = arr.reduce((acc, item) => {
-        if (item === ' ') {
-            switch (true) {
-                case acc.length == 0 || acc.at(-1).length > 0:
-                    acc.push([]);
-                    return acc;
-                case acc.at(-1).length == 0:
-                    return acc;
-            }
-        } else {
-            switch (true) {
-                case acc.length == 0:
-                    acc.push([]);
-                    acc[0].push(item.toUpperCase());
-                    return acc;
-                case acc.at(-1).length == 0:
-                    acc.at(-1).push(item.toUpperCase());
-                    return acc;
-                case acc.at(-1).length > 0:
-                    acc.at(-1).push(item);
-                    return acc;
-            }
-        }
-    }, []);
-    words = words.reduce((acc, item) => {
-        if (item.length > 0) {
-            acc.push(item.join(''));
-        }
-        return acc;
-    }, []);
-    return words.join(' ');
-}
+// export const upperFirst = (str) => {
+//     ... // Ваш код.
+// }
 
 console.log(upperFirst('1234'));
 console.log(upperFirst('hello world'));
